b112b809f790c7551e6f690a0819edb8db23fc01
KarlTamberg <tamberg.karl@gmail.com>
2022-11-23 18:55:59 +0200

    Autorun script update;

--- autorun.sh
+++ autorun.sh
@@ -1,11 +1,10 @@
-cd ~
 apt-get update
-apt-get upgrade
-apt-get install git
+apt-get upgrade -y
+apt-get install git -y
 git clone https://github.com/katamb/debian-cis.git
 cd debian-cis
 cp debian/default /etc/default/cis-hardening
 sed -i "s#CIS_ROOT_DIR=.*#CIS_ROOT_DIR='$(pwd)'#" /etc/default/cis-hardening
-./bin/hardening.sh --audit --allow-unsupported-distribution
-./bin/hardening.sh --set-hardening-level 5 --allow-unsupported-distribution
+#./bin/hardening.sh --audit-all-enable-passed --allow-unsupported-distribution
+./bin/hardening.sh --set-hardening-level 4 --allow-unsupported-distribution
 ./bin/hardening.sh --apply --allow-unsupported-distribution
